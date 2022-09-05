519b6874fa4ec63ae0e631081d73bd4e1285b7a3
Celine Vergara <111476474+megoohmi@users.noreply.github.com>
2022-09-05 09:58:25 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+
+    Student Me = new Student();
+
+    Me.surname = "Vergara";
+    Me.firstName = "Celine Margarette";
+    Me.middleInitial = "A";
+    Me.dateOfBirth = "11/28/2003";
+    Me.studentNumber = 2022101206;
+    Me.studentEmailAddress = "vergaraca@students.national-u.edu.ph";
+    Me.iAmAwesome = true;
+    Me.sayMyStudentNumber();
+    Me.sayMyEmailAddress();
+    Me.amIAwesome();
+
+    }
+}
