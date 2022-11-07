f42a7e1f86e77a13ffa4213cca59966d8de492d2
Celine Vergara <111476474+megoohmi@users.noreply.github.com>
2022-11-07 10:09:58 +0800

    Create FinalExam.java

---
+++ final_exam/FinalExam.java
@@ -0,0 +1,237 @@
+public class FinalExam {
+    public static void main(String[] args) throws Exception {
+
+        char[][] array = {
+                { 'R', 'E', 'I', 'M', 'A', 'E', 'R', 'S', 'G', 'T' },
+                { 'E', 'V', 'D', 'B', 'D', 'V', 'I', 'A', 'B', 'I' },
+                { 'E', 'L', 'R', 'O', 'F', 'S', 'A', 'W', 'U', 'K' },
+                { 'B', 'G', 'C', 'E', 'I', 'L', 'C', 'J', 'H', 'T' },
+                { 'S', 'S', 'K', 'G', 'D', 'Z', 'H', 'S', 'T', 'O' },
+                { 'V', 'M', 'J', 'A', 'V', 'H', 'Z', 'S', 'I', 'K' },
+                { 'C', 'H', 'I', 'L', 'L', 'T', 'O', 'P', 'G', 'M' },
+                { 'Y', 'S', 'P', 'I', 'T', 'A', 'P', 'R', 'W', 'D' },
+                { 'G', 'B', 'D', 'S', 'T', 'K', 'P', 'W', 'S', 'O' },
+                { 'G', 'T', 'L', 'H', 'Q', 'X', 'Q', 'L', 'O', 'E' },
+        };
+
+        System.out.println(" ");
+        System.out.println("1. ALAK ");
+
+        for (int row = 0; row < 10; row++) {
+            System.out.println("");
+            for (int column = 0; column < 10; column++) {
+                if (row == 7 && (column == 5 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 6 && (column == 4 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 5 && (column == 3 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 4 && (column == 2 )) {
+                    System.out.print(array[row][column]);
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+        }
+    
+        System.out.println(" ");
+        System.out.println("2. GITHUB ");
+
+        for (int row = 0; row < 10; row++) {
+            System.out.println("");
+            for (int column = 0; column < 10; column++) {
+                if (row == 6 && (column == 8 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 5 && (column == 8 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 4 && (column == 8 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 3 && (column == 8 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 2 && (column == 8 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 1 && (column == 8 )) {
+                    System.out.print(array[row][column]);
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+        }
+    
+        
+        System.out.println(" ");
+        System.out.println("3. SISIG ");
+
+        for (int row = 0; row < 10; row++) {
+            System.out.println("");
+            for (int column = 0; column < 10; column++) {
+                if (row == 0 && (column == 7 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 1 && (column == 6 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 2 && (column == 5 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 3 && (column == 4 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 4 && (column == 3 )) {
+                    System.out.print(array[row][column]);
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println("4. VSCODE ");
+
+        for (int row = 0; row < 10; row++) {
+            System.out.println("");
+            for (int column = 0; column < 10; column++) {
+                if (row == 5 && (column == 0 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 4 && (column == 1 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 3 && (column == 2 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 2 && (column == 3 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 1 && (column == 4 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 0 && (column == 5 )) {
+                    System.out.print(array[row][column]);
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+        }
+
+
+        System.out.println(" ");
+        System.out.println("5. BEER ");
+
+        for  (int row = 0; row < 4; row++) {
+            System.out.println("");
+            for (int column = 0; column < 10; column++) {
+                if (column == 0) {
+                    System.out.print(array[row][column]);
+                    
+                }
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println("6. JAVA ");
+
+        for (int row = 0; row < 10; row++) {
+            System.out.println("");
+            for (int column = 0; column < 10; column++) {
+                if (row == 3 && (column == 7 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 2 && (column == 6 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 1 && (column == 5 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 0 && (column == 4 )) {
+                    System.out.print(array[row][column]);
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println("7. TIKTOK ");
+
+        for (int row = 0; row < 6; row++) {
+            System.out.println("");
+            for (int column = 0; column < 10; column++) {
+                if (column == 9) {
+                    System.out.print(array[row][column]);
+                }
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println("8. CHILLTOP ");
+
+        for (int row = 0; row < 10; row++) {
+            System.out.println("");
+            for (int column = 0; column < 8; column++) {
+                if (row == 6) {
+                    System.out.print(array[row][column]);
+                }
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println("9. REDHORSE ");
+
+        for (int row = 0; row < 10; row++) {
+            System.out.println("");
+            for (int column = 0; column < 10; column++) {
+                if (row == 2 && (column == 2 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 3 && (column == 3 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 4 && (column == 4 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 5 && (column == 5 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 6 && (column == 6 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 7 && (column == 7 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 8 && (column == 8 )) {
+                    System.out.print(array[row][column]);
+                }
+                else if (row == 9 && (column == 9 )) {
+                    System.out.print(array[row][column]);
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+        }
+    
+        System.out.println(" ");
+        System.out.println("10. TIPSY ");
+
+        for (int row = 0; row < 10; row++) {
+            System.out.println("");
+            for (int column = 0; column < 5; column++) {
+                if (row == 7) {
+                    System.out.print(array[row][column]);
+                }
+            }
+        }
